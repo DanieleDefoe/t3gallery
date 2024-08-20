6965e6c92f9204a4b7d5bda576a15cf172f60bd3
Abuzar <abuzar.mamedov@yanex.ru>
2024-08-20 12:44:38 +0300

    feat: use auth on Homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,38 @@
 /* eslint-disable @next/next/no-img-element */
 
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const HomePage = async () => {
+const Images = async () => {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
 
   return (
+    <div className="flex flex-wrap gap-4">
+      {images.map((image, index) => (
+        <div key={index} className="flex w-48 flex-col gap-1">
+          <img src={image.url} alt={image.url} />
+          <p>{image.name}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const HomePage = async () => {
+  return (
     <main>
-      <div className="flex flex-wrap gap-4">
-        {images.map((image, index) => (
-          <div key={index} className="flex w-48 flex-col gap-1">
-            <img src={image.url} alt={image.url} />
-            <p>{image.name}</p>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-center text-2xl">
+          Please sign in above
+        </div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 };
